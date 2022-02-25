4c3934cb1f8c37bc68654f3171ea8b0e076f8300
MiloHoon <brainlessx3@gmail.com>
2022-02-26 01:29:21 +0800

    Build Exe

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-
     public void StartPlay()
     {
         SceneManager.LoadScene("GameScene");
     }
+
     public void Instruction()
     {
         SceneManager.LoadScene("InstructionScene");
     }
+
     public void AppQuit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
+
     public void InstructionQuit()
     {
         SceneManager.LoadScene("MainMenuScene");
